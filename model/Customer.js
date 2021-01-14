00e6e5a2fdca62d05918eed0d2b9875abe454e0c
hal-da <52889199+hal-da@users.noreply.github.com>
2021-01-14 15:14:44 +0100

    Update Customer.js

--- model/Customer.js
+++ model/Customer.js
@@ -1,32 +1,4 @@
 const mongoose = require('mongoose')
-
-/**
- * Kundenschema
-    Vorname: String
-    Nachname: String
-    Adresse: 	Straße/HausNr: String
-            PLZ: String
-            Ort: String
-    EmailAdr: String
-    Kunde seit: Date
-    letzter Einkauf: Date?
-
-    {
-    "firstName": "daniel",
-    "lastName": "huhu",
-    "adress": {
-        "street": "so",
-        "zip": "1234",
-        "city": "viena",
-        "country": "überall"
-    },
-    "email": "@sontgunh",
-    "customerSince": "23",
-    "lastPurchase": "12"
-}
-
- */
-
 const CustomerSchema = mongoose.Schema({
     firstName: {
         type: String,
